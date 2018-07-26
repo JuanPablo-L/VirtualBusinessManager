f0e0a2e3a325a13fc469520430dd34f833e67fd1
JuanPabloL <lopez.juanp@hotmail.com>
2018-07-25 23:14:10 -0300

    Initial Project

---
+++ src/main/java/com/jpl/vbm/VirtualBusinessManagerApplication.java
@@ -0,0 +1,12 @@
+package com.jpl.vbm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualBusinessManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualBusinessManagerApplication.class, args);
+	}
+}
